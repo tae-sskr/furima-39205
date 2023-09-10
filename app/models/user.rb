bee9e9d9b73df5ac1d185ef81227fd5670b3d5e2
tae-sskr <sasakura.tae@gmail.com>
2023-09-10 14:29:52 +0900

    パスワードへの追加バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
