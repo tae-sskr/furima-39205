a5bcd2fd2830fb912d8deefe3b7cec976f899bfa
tae-sskr <sasakura.tae@gmail.com>
2023-09-14 02:05:14 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :likes
-  has_one :order
+  # has_many :comments
+  # has_many :likes
+  # has_one :order
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :prefecture
+  belongs_to :schedule
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :schedule_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
