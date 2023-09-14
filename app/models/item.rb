253a4799e7d18a3b1206999b45ec0983ff32fd84
tae-sskr <sasakura.tae@gmail.com>
2023-09-14 14:58:57 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,12 @@ class Item < ApplicationRecord
   validates :shipping_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :schedule_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blamk" }
+  end
 
   validates: content, presence: true, unless: :was_attached?
   def was_attached?
