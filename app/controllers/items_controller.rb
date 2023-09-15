38dfd0d0d2133ae24215526b71cd5317ef93fd8a
tae-sskr <sasakura.tae@gmail.com>
2023-09-15 17:02:51 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :destroy]
 
   def index
-    @item = Item.all
+    # @item = Item.all
   end
 
   def new
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_id, :prefecture_id, :schedule_id, :price, :user, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_id, :prefecture_id, :schedule_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,10 +5,7 @@ window.addEventListener('load', function(){
   const profit = document.getElementById('profit');
 
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    addTax.innerHTML = (Math.floor(inputValue/10));
-    profit.innerHTML = inputValue - (Math.floor(inputValue/10));
-    console.log(inputValue);
+
   })
 
 })
