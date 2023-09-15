119293762e2ddc265409cd17c7b89035e617b305
tae-sskr <sasakura.tae@gmail.com>
2023-09-15 17:25:58 +0900

    修正2

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,7 +5,9 @@ window.addEventListener('load', function(){
   const profit = document.getElementById('profit');
 
   priceInput.addEventListener("input", () => {
-
+    const inputValue = priceInput.value;
+    addTax.innerHTML = (Math.floor(inputValue/10));
+    profit.innerHTML = inputValue - (Math.floor(inputValue/10));
   })
 
 })
