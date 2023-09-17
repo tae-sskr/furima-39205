eca5cb73c9f161a6acf693b41021db72ef37332f
tae-sskr <sasakura.tae@gmail.com>
2023-09-17 17:49:09 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @item = Item.all.order(created_at: :desc)
