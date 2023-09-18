156ed144f3649df42c03e1a6ccdb4f3344ce41e3
tae-sskr <sasakura.tae@gmail.com>
2023-09-18 16:16:54 +0900

    バリデーションの記載

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -3,6 +3,17 @@ class OrderShipping < ApplicationRecord
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :address1, :address2, :building_name, :telephone, :user_id, :item_id, :token
 
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :address1
+    validates :address2
+    validates :telephone, format: {with: /\A[0-9]{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
