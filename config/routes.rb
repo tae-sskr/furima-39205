6c8ba101dcfbc32463d97cfcd9ccd8dcb66ed7f1
tae-sskr <sasakura.tae@gmail.com>
2023-09-18 16:07:52 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  root to: "orders#index"
-  resources :items
-  resources :orders, only:[:create]
+  resources :items do
+    resources :orders
+  end
 end
