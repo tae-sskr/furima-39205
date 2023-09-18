0f20bd0f86d11b0eee5d45c2bd0556cc41c53d27
tae-sskr <sasakura.tae@gmail.com>
2023-09-18 11:36:03 +0900

    ordersテーブルにpriceカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2023_09_16_091205) do
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
+    t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
