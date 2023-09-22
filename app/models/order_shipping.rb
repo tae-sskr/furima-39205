75c014876a38c12d29c57487495c8f8263d1636c
tae-sskr <sasakura.tae@gmail.com>
2023-09-22 10:07:16 +0900

    Update order_shipping.rb

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -16,7 +16,6 @@ class OrderShipping
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   
   def save
-    binding.pry
     order = Order.create(user_id: user_id, item_id: item_id)
 
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, address1: address1, address2: address2, building_name: building_name, telephone: telephone, order_id: order.id)
